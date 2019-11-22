0bf2e4c7a3a9fbeae6b09200cd343d5ca35d87ef
ElAndyG <andrewgagne@outlook.com>
2019-11-22 15:04:42 -0500

    csharp core test suite setup

---
+++ elandyg/csharp/src.tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using src;
+
+namespace src.tests
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+    }
+}
